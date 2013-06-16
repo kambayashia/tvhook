4e228e15b50d1a53e3d5f691c4e450e9da75a6b7
kambayashia <kambayashia@debian7-64>
2013-06-16 13:42:38 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tvhook::Application.config.secret_key_base = '537afe2ccff7aa3db21ac75ef082cca28271af2155d7c173bf4e4c255b89444c565085e8c784982cd0334aad41ffd284ad35229e50b320fad71d09805ba1f27c'
